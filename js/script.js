b15658d1e02ba2a438dbf7feef27027c1d7e221c
Austin J <31734814+alj032@users.noreply.github.com>
2021-02-12 14:46:43 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,3 +27,19 @@ $(window).scroll(function() {
   });    
   
 }).scroll();
+
+// back to top button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
